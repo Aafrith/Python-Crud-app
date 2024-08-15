39d76c9b083c4877c13864aa481dc81810191491
Aafrith <139564438+Aafrith@users.noreply.github.com>
2024-08-15 18:44:46 +0530

    Read,delete,fonts and icons done

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -101,6 +101,34 @@ def update_record(request, pk):
 
     return render(request, 'webapp/update-record.html', context=context)
 
+
+# - Read / View a singular record
+
+@login_required(login_url='my-login')
+def singular_record(request, pk):
+
+    all_records = Record.objects.get(id=pk)
+
+    context = {'record': all_records}
+
+    return render(request, 'webapp/view-record.html', context=context)
+
+
+
+# - Delete a record
+
+@login_required(login_url='my-login')
+def delete_record(request, pk):
+
+    record = Record.objects.get(id=pk)
+
+    record.delete()
+
+    return redirect("dashboard")
+
+
+
+
 # - user logout
 
 def user_logout(request):
