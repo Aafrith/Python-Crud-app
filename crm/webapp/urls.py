951326552edc8fc97290c67fdc08e72336961e67
Aafrith <139564438+Aafrith@users.noreply.github.com>
2024-08-15 12:31:27 +0530

    CRUD-Create-Read1-Update

--- crm/webapp/urls.py
+++ crm/webapp/urls.py
@@ -12,9 +12,15 @@ urlpatterns = [
 
     path('user-logout', views.user_logout, name='user-logout'),
 
+    # CRUD
+
     path('dashboard', views.dashboard, name='dashboard'),
 
-    
+    path('create-record', views.create_record, name='create-record'),
+
+    path('update-record/<int:pk>', views.update_record, name='update-record'),
+
+
 
 
 ]
