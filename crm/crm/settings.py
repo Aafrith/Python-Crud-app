da1794c69cd89a9fc894410ba4cc8a3b4203a70d
Aafrith <139564438+Aafrith@users.noreply.github.com>
2024-08-15 09:57:41 +0530

    User creation and authentication

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -28,7 +28,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'webapp',
+
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
